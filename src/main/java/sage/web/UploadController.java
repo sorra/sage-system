572501847e56757ae7ce9fd53897b0afd971ff73
Sorra <dawnstar@163.com>
2014-10-26 12:17:39 +0800

    试做改进型 图片上传

--- src/main/java/sage/web/UploadController.java
+++ src/main/java/sage/web/UploadController.java
@@ -2,10 +2,10 @@ package sage.web;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.atomic.AtomicLong;
@@ -20,11 +20,11 @@ import sage.web.auth.Auth;
 @RestController
 @RequestMapping(value = "/upload", method = RequestMethod.POST)
 public class UploadController {
-  private static final long MAX_IMAGE_BYTES = 4*1024*1024;
-  
+  private static final long MAX_PICTURE_BYTES = 4*1024*1024;
   // Only for trial
-  private static final String UPLOAD_DIR = "D:/programs/upload";
-  // Only for trial
+  private static final String SUFFIX = ".jpg";
+  private static final String UPLOAD_DIR = "D:/programs/pic_upload";
+
   private AtomicLong maxNumber = new AtomicLong();
   {
     long max = 0;
@@ -33,7 +33,7 @@ public class UploadController {
       throw new RuntimeException("Cannot mkdir of " + UPLOAD_DIR);
     }
     for (String name : dir.list()) {
-      String numStr = name.substring(0, name.length() - ".jpg".length());
+      String numStr = name.substring(0, name.length() - SUFFIX.length());
       long current = Long.parseLong(numStr);
       if (current > max) {
         max = current;
@@ -42,33 +42,39 @@ public class UploadController {
     maxNumber.set(max);
   }
   
-  @RequestMapping("/image")
-  public Collection<String> uploadImage(@RequestParam MultipartFile[] files) throws IOException {
+  @RequestMapping("/picture")
+  public Collection<String> uploadPicture(@RequestParam MultipartFile[] files) throws IOException {
     Auth.checkCurrentUid();
     
     Collection<String> destinations = new ArrayList<>();    
     for (MultipartFile file : files) {
-      if (isImageFileSizeAllowed(file.getSize())) {
-        long number = maxNumber.incrementAndGet(); // Permanent increment, even if upload fails
-        Path path = Paths.get(UPLOAD_DIR + "/" + number + ".jpg");
-        try (InputStream in = file.getInputStream()) {
-          long written = Files.copy(in, path);
-          if (written == file.getSize()) {
-            destinations.add(number + ".jpg");
-          } else {
-            Files.delete(path);
-          }
+      if (!isPictureFileSizeAllowed(file.getSize())) {
+        throw new RuntimeException("图片文件太大了!");
+      }
+      byte[] bytes = file.getBytes();
+      // 后缀必须限定为图片格式，以防脚本注入攻击
+      String name = maxNumber.incrementAndGet() + SUFFIX;
+      Path path = Paths.get(UPLOAD_DIR + "/" + name);
+
+      Files.write(path, bytes, StandardOpenOption.CREATE_NEW);
+      destinations.add(name);
+
+    /*
+      try (InputStream in = file.getInputStream()) {
+        long written = Files.copy(in, path);
+        if (written == file.getSize()) {
+          destinations.add(number + ".jpg");
+        } else {
+          Files.delete(path);
         }
       }
+    */
     }
     
     return destinations;
   }
   
-  private boolean isImageFileSizeAllowed(long size) {
-    if (size > 0 && size <= MAX_IMAGE_BYTES) {
-      return true;
-    }
-    return false;
+  private boolean isPictureFileSizeAllowed(long size) {
+    return size > 0 && size <= MAX_PICTURE_BYTES;
   }
 }
