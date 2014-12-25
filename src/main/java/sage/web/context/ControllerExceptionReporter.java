5321678f5a6b6bf3b2b6adacb93af1058ad2c28e
Sorra <dawnstar@163.com>
2014-12-25 15:18:22 +0800

    Improved controller exception handling

--- src/main/java/sage/web/context/ControllerExceptionReporter.java
+++ src/main/java/sage/web/context/ControllerExceptionReporter.java
@@ -2,19 +2,28 @@ package sage.web.context;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 @Order(Ordered.LOWEST_PRECEDENCE)
 public class ControllerExceptionReporter {
-  private final Logger logger = LoggerFactory.getLogger(getClass());
+  private final Logger log = LoggerFactory.getLogger(getClass());
+
+  @ExceptionHandler(TypeMismatchException.class)
+  @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Parameter type mismatch")
+  public void typeMismatch(TypeMismatchException e) {
+    log.error(e.toString());
+  }
 
   @ExceptionHandler
-  public void logAny(Throwable e) throws Throwable {
-    logger.error("controller error encountered");
+  public void any(Throwable e) throws Throwable {
+    log.error("controller error encountered");
     throw e;
   }
 }
