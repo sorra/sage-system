b18c163ee9e9e88063fe65d4872a94ce8f9a6ae7
Sorra <dawnstar@163.com>
2015-02-09 15:42:07 +0800

    Upload directory automatically tries Windows(C:, D:) or Unix(user.home) as root directory.

--- src/main/java/sage/web/UploadController.java
+++ src/main/java/sage/web/UploadController.java
@@ -10,6 +10,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,18 +22,33 @@ import sage.web.auth.Auth;
 @RestController
 @RequestMapping(value = "/upload", method = RequestMethod.POST)
 public class UploadController {
+  private static final Logger log = LoggerFactory.getLogger(UploadController.class);
+
   private static final long MAX_PICTURE_BYTES = 4*1024*1024;
   // Only for trial
   private static final String SUFFIX = ".jpg";
-  private static final String UPLOAD_DIR = "D:/programs/pic_upload";
+  private static final String[] DIR_ROOTS = {"C:", "D:", System.getProperty("user.home")};
+  private static final String SUBDIR = "/programs/pics_upload";
+  private String UPLOAD_DIR;
 
   private AtomicLong maxNumber = new AtomicLong();
   {
-    long max = 0;
-    File dir = new File(UPLOAD_DIR);
-    if (!dir.exists() && !dir.mkdirs()) {
-      throw new RuntimeException("Cannot mkdir of " + UPLOAD_DIR);
+    File dir = null;
+    for (String root : DIR_ROOTS) {
+      if (new File(root).exists()) {
+        UPLOAD_DIR = root + SUBDIR;
+        dir = new File(UPLOAD_DIR);
+        if ((dir.exists() || dir.mkdirs()) && dir.canWrite()) {
+          log.info("Select the Upload Directory: " + UPLOAD_DIR);
+          break;
+        }
+      }
     }
+    if (dir == null) {
+      throw new RuntimeException("Cannot create upload directory! user.home=" + System.getProperty("user.home"));
+    }
+
+    long max = 0;
     for (String name : dir.list()) {
       String numStr = name.substring(0, name.length() - SUFFIX.length());
       long current = Long.parseLong(numStr);
