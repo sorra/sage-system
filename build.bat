8627230283ecc7b4387e6d0a5e7047c85bec5d9a
Sorra <dawnstar@163.com>
2015-02-12 21:01:26 +0800

    Deploy to ROOT, so that become Tomcat context root

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 echo Begin build...
 call mvn clean package
-rd /s /q %TOMCAT_HOME%\webapps\sage\
-xcopy /Y %cd%\target\sage %TOMCAT_HOME%\webapps\sage\ /e /q
+rd /s /q %TOMCAT_HOME%\webapps\ROOT\
+xcopy /Y %cd%\target\sage %TOMCAT_HOME%\webapps\ROOT\ /e /q
 echo Build OK!
