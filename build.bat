4f0d8d0e1816569839220a133809f738219e8434
Sorra <dawnstar@163.com>
2015-02-28 21:37:37 +0800

    echo

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 echo Begin build...
 call mvn clean package
+echo Clean-copy to TOMCAT...
 rd /s /q %TOMCAT_HOME%\webapps\ROOT\
 xcopy /Y %cd%\target\sage %TOMCAT_HOME%\webapps\ROOT\ /e /q
 echo Build OK!
