560e20f9175de07ed47e16d43daa1d1184e85d83
Sorra <dawnstar@163.com>
2015-03-19 23:52:22 +0800

    注册页面已正常，并验证输入

---
+++ src/main/webapp/js/register.js
@@ -0,0 +1,47 @@
+"use strict";
+$(document).ready(function() {
+  $('form').submit(function(event) {
+    try {
+      var valid = validate()
+    } catch (e) {
+      event.preventDefault()
+      alert('注册未能成功: ' + e)
+      throw e
+    }
+    if (!valid ) {
+      event.preventDefault()
+    }
+  })
+})
+
+function validate() {
+  var count = 0
+  function alertNode(text) {
+    count++
+    return $('<span class="validation-alert"></span>').text(text)
+  }
+  $('.validation-alert').remove()
+
+  var email = $('#email').val()
+  if (email.length == 0) {
+    alertNode('请输入内容').insertAfter($('#email'))
+  }
+  var idxOfAt = email.indexOf('@')
+  if (idxOfAt <= 0 || email.indexOf('.', idxOfAt) <= 0) {
+    alertNode('请输入有效的邮箱地址').insertAfter($('#email'))
+  }
+
+  var password = $('#password').val()
+  if(password.length == 0) {
+    alertNode('请输入内容').insertAfter($('#password'))
+  }
+  if (password.length < 8 || password.length > 20) {
+    alertNode('密码应为8~20位').insertAfter($('#password'))
+  }
+
+  var repeat = $('#repeat-password').val()
+  if (password != repeat) {
+    alertNode('两次密码输入不一致').insertAfter($('#repeat-password'))
+  }
+  return count == 0
+}
