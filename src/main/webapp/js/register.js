965d0318a3fa79b02a606606c4710de67057c534
Sorra <dawnstar@163.com>
2015-03-21 14:52:44 +0800

    No redundancy in validation error messages

--- src/main/webapp/js/register.js
+++ src/main/webapp/js/register.js
@@ -25,23 +25,26 @@ function validate() {
   var email = $('#email').val()
   if (email.length == 0) {
     alertNode('请输入内容').insertAfter($('#email'))
-  }
-  var idxOfAt = email.indexOf('@')
-  if (idxOfAt <= 0 || email.indexOf('.', idxOfAt) <= 0) {
-    alertNode('请输入有效的邮箱地址').insertAfter($('#email'))
+  } else {
+    var idxOfAt = email.indexOf('@')
+    if (idxOfAt <= 0 || email.indexOf('.', idxOfAt) <= 0) {
+      alertNode('请输入有效的邮箱地址').insertAfter($('#email'))
+    }
   }
 
   var password = $('#password').val()
   if(password.length == 0) {
     alertNode('请输入内容').insertAfter($('#password'))
-  }
-  if (password.length < 8 || password.length > 20) {
-    alertNode('密码应为8~20位').insertAfter($('#password'))
+  } else {
+    if (password.length < 8 || password.length > 20) {
+      alertNode('密码应为8~20位').insertAfter($('#password'))
+    }
   }
 
   var repeat = $('#repeat-password').val()
   if (password != repeat) {
     alertNode('两次密码输入不一致').insertAfter($('#repeat-password'))
   }
+  
   return count == 0
 }
