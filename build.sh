8229305b1cf7793607c24f8f47e034f1b27f9391
Hu Dongqing <dhu>
2015-07-03 20:30:11 +0800

    Shell script 前后命令串联

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 echo Build begins...
-mvn clean package
-echo Clean-copy to TOMCAT...
-rm -rf $TOMCAT_HOME/webapps/ROOT
-cp -r target/sage $TOMCAT_HOME/webapps/ROOT
-echo Build done.
+mvn clean package \
+&& echo Clean-copy to TOMCAT... \
+&&rm -rf $TOMCAT_HOME/webapps/ROOT \
+&& cp -r target/sage $TOMCAT_HOME/webapps/ROOT \
+&& echo Build done.
