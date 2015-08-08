2a6a3f9bab0fb9b04b14bc76f15081782b30b606
Hu Dongqing <dhu>
2015-08-08 14:46:27 +0800

    人性化日期输出 humanTime

---
+++ src/main/java/sage/web/context/DateUtil.java
@@ -0,0 +1,29 @@
+package sage.web.context;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+public class DateUtil {
+  public static String humanTime(Date time) {
+    Instant instant = Instant.ofEpochMilli(time.getTime());
+    long minutes = instant.until(Instant.now(), ChronoUnit.MINUTES);
+    LocalDateTime thatTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+    LocalDate thatDay = thatTime.toLocalDate();
+
+    if (minutes >= 0 && minutes < 60) {
+      return minutes + "分钟前";
+    }
+    if (LocalDate.now().isEqual(thatDay)) { // 当天
+      return DateTimeFormatter.ofPattern("HH:mm").format(thatTime);
+    }
+    if (LocalDate.now().getYear() == thatDay.getYear()) { // 当年
+      return DateTimeFormatter.ofPattern("MM-dd HH:mm").format(thatTime);
+    }
+    return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm").format(thatTime);
+  }
+}
