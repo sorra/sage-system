3dc9b942845646763d9f33e15767e1db7f3c24de
Hu Dongqing <dhu>
2015-08-23 22:57:33 +0800

    要既能让Ajax知道出错了，又能让普通访问重定向到登录页

--- src/main/java/sage/web/auth/AuthExceptionHandler.java
+++ src/main/java/sage/web/auth/AuthExceptionHandler.java
@@ -3,18 +3,22 @@ package sage.web.auth;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 @Order(0)
 public class AuthExceptionHandler {
   @ExceptionHandler(RequireLoginException.class)
-  public String redirectToLogin(HttpServletRequest request) {
+  @ResponseStatus(HttpStatus.UNAUTHORIZED)
+  public ModelAndView redirectToLogin(HttpServletRequest request) {
     String uri = request.getRequestURI();
     if (request.getQueryString() != null) {
       uri += ("?" + request.getQueryString());
     }
-    return "redirect:/login?" + Auth.getRedirectGoto(uri);
+    return new ModelAndView("error-no-login", "location", "/login?" + Auth.getRedirectGoto(uri));
   }
 }
