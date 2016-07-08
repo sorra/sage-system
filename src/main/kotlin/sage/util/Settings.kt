b2b5ce16ffad4e6e47bceb9255d461021dd20c66
Dongqing Hu <sorra@outlook.com>
2016-07-08 21:40:35 +0800

    use CDN

---
+++ src/main/kotlin/sage/util/Settings.kt
@@ -0,0 +1,17 @@
+package sage.util
+
+import java.io.File
+import java.io.FileInputStream
+import java.util.*
+
+
+object Settings {
+  val props = {
+    val path = System.getProperty("user.home") + "/sage-settings.properties"
+    val properties = Properties()
+    if (File(path).exists()) {
+      properties.load(FileInputStream(path))
+    }
+    properties
+  }.invoke()
+}
--- src/main/kotlin/sage/web/context/RenderUtil.kt
+++ src/main/kotlin/sage/web/context/RenderUtil.kt
@@ -1,7 +1,12 @@
 package sage.web.context
 
 import sage.domain.commons.ReformMention
+import sage.util.Settings
 
 object RenderUtil {
+  val CDN = Settings.props.getProperty("cdn")
+
+  @JvmStatic fun cdn() = CDN
+
   @JvmStatic fun reformMention(text: String) = ReformMention.apply(text)
 }
