e7522392fe98aa791d141894a14b2d75255b5a8a
Dongqing Hu <sorra@outlook.com>
2016-12-17 19:11:57 +0800

    use glyphicon buttons

--- src/main/webapp/static/js/read.js
+++ src/main/webapp/static/js/read.js
@@ -2,14 +2,17 @@ function btnLike_init() {
 
   function refreshLikes($btn, prefix) {
     return $.get(prefix + '/likes').done(function(likes){
+      if (likes.toString() == '0') {
+        likes = ''
+      }
       $btn.find('.num-likes').text(likes)
     })
   }
 
-  $(document).on('click', '.btn_like', function(){
+  $(document).on('click', '.like-btn', function(){
     var $btn = $(this)
     var prefix = $btn.data('prefix')
-    var classLiked = 'btn-success'
+    var classLiked = 'like-btn-liked'
     if ($btn.hasClass(classLiked)) {
       $.post(prefix + '/unlike').done(function(){
         refreshLikes($btn, prefix)
