24178665d05a8cd2f3400adddea7c845fb1cb202
Dongqing Hu <sorra@outlook.com>
2017-01-12 22:02:37 +0800

    update the build script (sage 0.3)

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-./gradlew clean webmin build && mkdir build/app && cd build/app && jar xf ../libs/sage-0.2-SNAPSHOT.jar && cp -r ../../src/main/webapp public && cp ../../*.sh .
+./gradlew clean webmin build && mkdir build/app && cd build/app && jar xf ../libs/sage-0.3-SNAPSHOT.jar && cp -r ../../src/main/webapp public && cp ../../*.sh .
