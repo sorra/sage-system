1df69035c0dd1563680e7611b0fc3c85e32fcfc9
Dongqing Hu <sorra@outlook.com>
2017-04-11 20:50:11 +0800

    Allow style attribute for rich-text editor

--- src/main/kotlin/sage/util/JsoupUtil.kt
+++ src/main/kotlin/sage/util/JsoupUtil.kt
@@ -6,6 +6,16 @@ import org.jsoup.safety.Whitelist
 object JsoupUtil {
   // Though adding "#" and "/" has no effect
   val whitelist: Whitelist = Whitelist.relaxed().addProtocols("a", "href", "#", "/")
+  // Allow style attribute for rich-text editor
+  init {
+    listOf("a", "b", "blockquote", "br", "caption", "cite", "code", "col",
+    "colgroup", "dd", "div", "dl", "dt", "em", "h1", "h2", "h3", "h4", "h5", "h6",
+    "i", "img", "li", "ol", "p", "pre", "q", "small", "span", "strike", "strong",
+    "sub", "sup", "table", "tbody", "td", "tfoot", "th", "thead", "tr", "u",
+    "ul").forEach { tag ->
+      whitelist.addAttributes(tag, "style")
+    }
+  }
 
   fun clean(html: String): String = Jsoup.clean(html, whitelist)
 }
