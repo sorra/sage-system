1635ab37d86d0fc942848d93d4e8d638c2b6a41a
Dongqing Hu <sorra@outlook.com>
2017-07-15 15:48:55 +0800

    Correct database-and-user.sql

--- database-and-user.sql
+++ database-and-user.sql
@@ -1,2 +1,2 @@
-CREATE DATABASE `bage` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-GRANT ALL ON `bage`.* TO `bage`@localhost IDENTIFIED BY '1234';
+CREATE DATABASE `sage` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+GRANT ALL ON `sage`.* TO `bage`@localhost IDENTIFIED BY '1234';
