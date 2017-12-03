1ef12efe942acf1584cf128a58bfc437993b2cea
Dongqing Hu <sorra@outlook.com>
2017-12-03 15:43:23 +0800

    Advance binary versions

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-./gradlew clean webmin build && mkdir build/app && cd build/app && jar xf ../libs/sage-0.3-SNAPSHOT.jar \
+./gradlew clean webmin build && mkdir build/app && cd build/app && jar xf ../libs/sage-1.0-SNAPSHOT.jar \
 && cp -r ../../src/main/webapp public && cp ../../*.sh . && cp ../../production-files/* .
